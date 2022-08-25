a2f0e79e7ec56637f410ee9fffcabdf1fa76d960
DevPrinceK <princesamuelpks@gmail.com>
2022-08-25 20:49:16 +0000

    Update views.py

--- core/api/views.py
+++ core/api/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.views import View
+from rest_framework import generics, permissions
+from rest_framework.authtoken.serializers import AuthTokenSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import status
+from django.utils.decorators import method_decorator
+from rest_framework.views import APIView
 
-# Create your views here.
+
+# @method_decorator(api_view(['GET']))
+class HomeAPIView(APIView):
+    def get(self, request, *args, **kwargs):
+        end_points = {
+            'auth': '/api/auth/',
+            'users': '/api/users/',
+            'groups': '/api/groups/',
+            'permissions': '/api/permissions/',
+        }
+        return Response(end_points)
