2b2745fac2e97e47ef50e51285051e22ddb448b7
DevPrinceK <princesamuelpks@gmail.com>
2022-08-26 13:35:10 +0000

    Update admin.py

--- core/backend/admin.py
+++ core/backend/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+admin.site.register(Trip)
+admin.site.register(Vehicle)
+admin.site.register(VehicleCategory)
+admin.site.register(Agency)
