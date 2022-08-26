04300d200ae8e17abc9f0a2ea0d2e2ac1fbf274b
DevPrinceK <princesamuelpks@gmail.com>
2022-08-26 13:34:58 +0000

    Update urls.py

--- core/api/urls.py
+++ core/api/urls.py
@@ -4,4 +4,8 @@ from . import views
 app_name = "api"
 urlpatterns = [
     path("", views.HomeAPIView.as_view(), name="home"),
+    path('sign-up/', views.SignUpAPI.as_view(), name="sign_up"),
+    path('all-trips/', views.AllTripsAPI.as_view(), name="all_trips"),
+    path('trips-today/', views.TripsTodayAPI.as_view(), name="trips_today"),
+    path('trips-twsad/', views.TripsTodayWithSourceAndDestinationAPI.as_view(), name="trips_twsad"),  # noqa
 ]
