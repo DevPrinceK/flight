ae6c1e0c8b52b923886fb73aacc5820bb150db94
DevPrinceK <princesamuelpks@gmail.com>
2022-08-27 16:54:03 +0000

    Create __init__.py

---
+++ core/backend/views/__init__.py
@@ -0,0 +1,16 @@
+from .agency import *
+from .booking import *
+from .category import *
+from .seat import *
+from .trip import *
+from .vehicle import *
+from .ticket import *
+from .transaction import *
+
+
+class DashboardView(View):
+    template = "backend/index.html"
+
+    def get(self, request, *args, **kwargs):
+        context = {}
+        return render(request, self.template, context)
