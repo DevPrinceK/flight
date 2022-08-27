a15162bf6c845f0f9bdc6a4d5588a1de3728bbec
DevPrinceK <princesamuelpks@gmail.com>
2022-08-27 14:36:26 +0000

    Update urls.py

--- core/backend/urls.py
+++ core/backend/urls.py
@@ -6,8 +6,22 @@ urlpatterns = [
     path("", views.DashboardView.as_view(), name="backend"),
 ]
 
-
 # VEHICLES
 urlpatterns += [
     path("vehicles/", views.VehicleListView.as_view(), name="vehicles"),
+]
+
+# TRANSACTIONS
+urlpatterns += [
+    path("transactions/", views.TransactionListView.as_view(), name="transactions"),
+]
+
+# BOOKINGS
+urlpatterns += [
+    path("bookings/", views.BookingListView.as_view(), name="bookings"),
+]
+
+# TICKETS
+urlpatterns += [
+    path("tickets/", views.TicketListView.as_view(), name="tickets"),
 ]
