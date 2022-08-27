f09f9bd082f57df68be1205b3b07896e2dafc3c8
DevPrinceK <princesamuelpks@gmail.com>
2022-08-27 16:48:29 +0000

    Create forms.py

---
+++ core/backend/forms.py
@@ -0,0 +1,37 @@
+from django import forms
+
+from .models import Agency
+from .models import Vehicle
+from .models import Seat
+from .models import VehicleCategory
+from .models import Trip
+
+
+class AgencyForm(forms.ModelForm):
+    class Meta:
+        model = Agency
+        fields = ['name', 'address', 'phone', 'email', 'website']
+
+
+class VehicleForm(forms.ModelForm):
+    class Meta:
+        model = Vehicle
+        fields = ['name', 'vin']
+
+
+class SeatForm(forms.ModelForm):
+    class Meta:
+        model = Seat
+        fields = ['seat_num']
+
+
+class VehicleCategoryForm(forms.ModelForm):
+    class Meta:
+        model = VehicleCategory
+        fields = ['name']
+
+
+class TripForm(forms.ModelForm):
+    class Meta:
+        model = Trip
+        fields = ['source', 'destination', 'date', 'time']
