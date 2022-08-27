3c5b65f6aa7b16fd61497b1332d85bbf18acd951
DevPrinceK <princesamuelpks@gmail.com>
2022-08-27 16:10:09 +0000

    Update views.py

--- core/backend/views.py
+++ core/backend/views.py
@@ -5,6 +5,9 @@ from backend.models import Vehicle
 from backend.models import Transaction
 from backend.models import Booking
 from backend.models import Ticket
+from backend.models import Seat
+from backend.models import Trip
+from backend.models import VehicleCategory
 
 
 class DashboardView(View):
@@ -48,4 +51,31 @@ class TicketListView(View):
     def get(self, request, *args, **kwargs):
         tickets = Ticket.objects.all().order_by('-id')
         context = {'tickets': tickets}
+        return render(request, self.template, context)
+
+
+class SeatListView(View):
+    template = "backend/lists/seats.html"
+
+    def get(self, request, *args, **kwargs):
+        seats = Seat.objects.all().order_by('-id')
+        context = {'seats': seats}
+        return render(request, self.template, context)
+
+
+class TripListView(View):
+    template = "backend/lists/trips.html"
+
+    def get(self, request, *args, **kwargs):
+        trips = Trip.objects.all().order_by('-id')
+        context = {'trips': trips}
+        return render(request, self.template, context)
+
+
+class CategoryListView(View):
+    template = "backend/lists/categories.html"
+
+    def get(self, request, *args, **kwargs):
+        categories = VehicleCategory.objects.all().order_by('-id')
+        context = {'categories': categories}
         return render(request, self.template, context)
