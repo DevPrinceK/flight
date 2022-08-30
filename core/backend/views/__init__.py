c8e906205b59acaaa2810ea499b543c22b2ed403
DevPrinceK <princesamuelpks@gmail.com>
2022-08-30 23:47:03 +0000

    Update __init__.py

--- core/backend/views/__init__.py
+++ core/backend/views/__init__.py
@@ -11,6 +11,7 @@ from .transaction import *
 class DashboardView(View):
     template = "backend/index.html"
 
+    @method_decorator(MustLogin)
     def get(self, request, *args, **kwargs):
         context = {}
         return render(request, self.template, context)
