9b0f8bca510fe3f596f1b9cef79b72dd1e16b274
DevPrinceK <princesamuelpks@gmail.com>
2022-08-30 23:46:21 +0000

    Update urls.py

--- core/accounts/urls.py
+++ core/accounts/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 app_name = "accounts"
 urlpatterns = [
+    path("", views.LoginView.as_view(), name="login"),
     path("login/", views.LoginView.as_view(), name="login"),
     path("logout/", views.LogoutView.as_view(), name="logout"),
     path("register-agency/", views.RegisterAgencyView.as_view(), name="register_agency"),  # noqa
