dda69a3d52026a9fe5b0a14258d4e452e2f2aa56
DevPrinceK <princesamuelpks@gmail.com>
2022-08-30 23:49:02 +0000

    Update urls.py

--- core/core/urls.py
+++ core/core/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
-    path('', include('backend.urls')),
+    path('backend/', include('backend.urls')),
     path('', include('accounts.urls')),
 ]
 
