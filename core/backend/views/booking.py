1cbdc2f465d130186578e143deede355a2c5ad4c
DevPrinceK <princesamuelpks@gmail.com>
2022-08-30 23:47:19 +0000

    Update booking.py

--- core/backend/views/booking.py
+++ core/backend/views/booking.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from django.views import View
+from django.utils.decorators import method_decorator
 from backend.models import Booking
+
+from core.utils.decorators import MustLogin
+
 
 class BookingListView(View):
     template = "backend/lists/bookings.html"
 
+    @method_decorator(MustLogin)
     def get(self, request, *args, **kwargs):
         bookings = Booking.objects.all().order_by('-id')
         context = {'bookings': bookings}
