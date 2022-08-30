a9406c3aaec33a28a77f34b445baf6c11062cca6
DevPrinceK <princesamuelpks@gmail.com>
2022-08-30 23:47:45 +0000

    Update ticket.py

--- core/backend/views/ticket.py
+++ core/backend/views/ticket.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 from django.views import View
-
+from django.utils.decorators import method_decorator
+from core.utils.decorators import MustLogin
 from backend.models import Ticket
 
 
 class TicketListView(View):
     template = "backend/lists/tickets.html"
 
+    @method_decorator(MustLogin)
     def get(self, request, *args, **kwargs):
         tickets = Ticket.objects.all().order_by('-id')
         context = {'tickets': tickets}
