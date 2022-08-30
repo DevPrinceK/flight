444adc000166fbbd4faa9fd98ea1f4d749078236
DevPrinceK <princesamuelpks@gmail.com>
2022-08-30 19:51:22 +0000

    Update views.py

--- core/accounts/views.py
+++ core/accounts/views.py
@@ -1,7 +1,80 @@
-from django.shortcuts import render, HttpResponse
+from http.client import HTTPResponse
+from django.shortcuts import render, redirect
 from django.views import View
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+import datetime
 
 
-class AccountLoginPage(View):
+from backend.forms import AgencyForm
+from accounts.manager import AccountManager
+
+
+class RegisterAgencyView(View):
+    template = "accounts/register.html"
+
     def get(self, request, *args, **kwargs):
-        return HttpResponse("Login Page")
+        return render(request, self.template, {})
+
+    def post(self, request, *args, **kwargs):
+        form = AgencyForm(request.POST, request.FILES or None)
+        user_email = request.POST.get("user_email")
+        user_password = request.POST.get("password")
+        first_name = request.POST.get("firstname")
+        last_name = request.POST.get("lastname")
+        if form.is_valid():
+            agency = form.save()
+            if agency:
+                # agency is created so go ahead and create an admin use for agency
+                user = AccountManager().create_agency_admin(
+                    email=user_email,
+                    password=user_password,
+                )
+                if user:
+                    # user is created so go ahead and assign agency to user
+                    user.agency = agency
+                    user.first_name = first_name
+                    user.last_name = last_name
+                    user.save()
+                    # login user after a successful registration
+                    user = authenticate(
+                        email=user_email,
+                        password=user_password,
+                    )
+                    if user:
+                        login(request, user)
+                        return redirect("backend:dashboard")
+                    else:
+                        return HTTPResponse("Something went wrong! Contact admins.")
+                    return redirect("accounts:login")
+        messages.success(request, "Agency Not Created Contact Admins!")
+        return redirect("accounts:login")
+
+
+class LoginView(View):
+    template = 'accounts/login.html'
+
+    def get(self, request, *args, **kwargs):
+        context = {}
+        return render(request, self.template, context)
+
+    def post(self, request, *args, **kwargs):
+        username = request.POST.get('email')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            user.last_login = datetime.datetime.now()
+            messages.success(request, 'Hi there, {}'.format(user.name))
+            return redirect('backend:dashboard')
+        else:
+            messages.error(request, 'Invalid credentials')
+            return redirect('accounts:login')
+
+
+class LogoutView(View):
+    def get(self, request, *args, **kwargs):
+        logout(request)
+        messages.success(request, 'You have been logged out')
+        return redirect('accounts:login')
