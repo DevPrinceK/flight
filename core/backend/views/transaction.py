ce42e52aa67cef0bbea8dc4084b232d0577eef71
DevPrinceK <princesamuelpks@gmail.com>
2022-08-30 23:47:55 +0000

    Update transaction.py

--- core/backend/views/transaction.py
+++ core/backend/views/transaction.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.views import View
+from django.utils.decorators import method_decorator
+from core.utils.decorators import MustLogin
 
 from backend.models import Transaction
 
@@ -7,6 +9,7 @@ from backend.models import Transaction
 class TransactionListView(View):
     template = "backend/lists/transactions.html"
 
+    @method_decorator(MustLogin)
     def get(self, request, *args, **kwargs):
         transactions = Transaction.objects.all().order_by('-id')
         context = {'transactions': transactions}
