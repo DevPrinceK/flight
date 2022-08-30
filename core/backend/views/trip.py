c98ec453d450fa20ce15c2ee79befafa8289adbb
DevPrinceK <princesamuelpks@gmail.com>
2022-08-30 19:53:34 +0000

    Update trip.py

--- core/backend/views/trip.py
+++ core/backend/views/trip.py
@@ -1,6 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
-from backend.models import Trip
+from django.contrib import messages
+from django.contrib.auth.mixins import PermissionRequiredMixin
+from django.http import HttpResponseRedirect
+from django.utils.html import strip_tags
+from backend.forms import TripForm
+
+from backend.models import Trip, Vehicle
 
 
 class TripListView(View):
@@ -10,3 +16,72 @@ class TripListView(View):
         trips = Trip.objects.all().order_by('-id')
         context = {'trips': trips}
         return render(request, self.template, context)
+
+
+class CreateUpdateTrip(View):
+    template = "backend/create_update_trip.html"
+
+    def get(self, request, *args, **kwargs):
+        trip_id = request.GET.get('trip_id')
+        vehicle_id = request.GET.get('vehicle_id')
+
+        trip = Trip.objects.filter(id=trip_id).first()
+        vehicle = Vehicle.objects.filter(id=vehicle_id).first()
+        vehicles = Vehicle.objects.all()
+
+        vehicles = Vehicle.objects.all()
+        context = {
+            "vehicle": vehicle,
+            "vehicles": vehicles,
+            "trip": trip,
+        }  # noqa
+        return render(request, self.template, context)
+
+    def post(self, request, *args, **kwargs):
+        trip_id = request.POST.get('trip_id')
+        vehicle_id = request.POST.get('vehicle_id')
+        vehicle = Vehicle.objects.filter(id=vehicle_id).first()
+
+        if trip_id:
+            # trip exists - update trip
+            trip = Trip.objects.filter(id=trip_id).first()
+            form = TripForm(request.POST, request.FILES, instance=trip)  # noqa
+            if form.is_valid():
+                new_trip = form.save(commit=False)
+                new_trip.vehicle = vehicle
+                new_trip.save()
+                messages.success(request, 'Trip Detail Updated Successfully.')  # noqa
+                return redirect('backend:trips')
+            else:
+                for field, error in form.errors.items():
+                    message = f"{field.title()}: {strip_tags(error)}"
+                    break
+                messages.warning(request, message)
+                return HttpResponseRedirect(request.META.get("HTTP_REFERER"))
+        else:
+            # it's a new trip - create trip
+            form = TripForm(request.POST, request.FILES)
+            if form.is_valid():
+                seat = form.save(commit=False)
+                seat.vehicle = vehicle
+                seat.save()
+                messages.success(request, 'New Trip Created Successfully.')  # noqa
+                return redirect('backend:trips')
+            else:
+                for field, error in form.errors.items():
+                    message = f"{field.title()}: {strip_tags(error)}"
+                    break
+                messages.warning(request, message)
+                return HttpResponseRedirect(request.META.get("HTTP_REFERER"))
+
+
+class DeleteTrip(View):
+    def get(self, request, *args, **kwargs):
+        return redirect('backend:trips')
+
+    def post(self, request, *args, **kwargs):
+        trip_id = request.POST.get('trip_id')
+        trip = Trip.objects.filter(id=trip_id).first()
+        trip.delete()
+        messages.success(request, 'Trip Deleted Successfully.')
+        return HttpResponseRedirect(request.META.get("HTTP_REFERER"))
