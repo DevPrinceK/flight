e6c1605c50fd3a02604052d710c50f0da880f0b4
DevPrinceK <princesamuelpks@gmail.com>
2022-08-31 08:28:50 +0000

    implement permisions

--- core/backend/views/transaction.py
+++ core/backend/views/transaction.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
 from django.views import View
 from django.utils.decorators import method_decorator
+from django.contrib.auth.mixins import PermissionRequiredMixin
+
 from core.utils.decorators import MustLogin
 
 from backend.models import Transaction
 
 
-class TransactionListView(View):
+class TransactionListView(PermissionRequiredMixin, View):
+    permission_required = [
+        "backend.view_transaction",
+    ]
     template = "backend/lists/transactions.html"
 
     @method_decorator(MustLogin)
