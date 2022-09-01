6a5c443ba97890cabc7811e9c45aad06211896e0
DevPrinceK <princesamuelpks@gmail.com>
2022-09-01 00:21:39 +0000

    Update __init__.py

--- core/backend/views/__init__.py
+++ core/backend/views/__init__.py
@@ -6,6 +6,7 @@ from .trip import *
 from .vehicle import *
 from .ticket import *
 from .transaction import *
+from .users import *
 
 
 class DashboardView(View):
@@ -13,5 +14,38 @@ class DashboardView(View):
 
     @method_decorator(MustLogin)
     def get(self, request, *args, **kwargs):
-        context = {}
+        user_agency = request.user.agency
+        if request.user.is_staff or request.user.is_superuser:
+            total_users = User.objects.count()
+            agencies = Agency.objects.all().count()
+            vehicles = Vehicle.objects.all().count()
+            seats = Seat.objects.all().count()
+            trips = Trip.objects.all().count()
+            bookings = Booking.objects.all().count()
+            tickets = Ticket.objects.all().count()
+            transactions = Transaction.objects.all().count()
+        elif request.user.is_agency_admin:
+            total_users = User.objects.filter(agency=user_agency).count()  # noqa
+            agencies = Agency.objects.filter(id=user_agency.id).count()
+            vehicles = Vehicle.objects.filter(agency=user_agency).count()  # noqa
+            seats = Seat.objects.filter(vehicle__agency=user_agency).count()  # noqa
+            trips = Trip.objects.filter(vehicle__agency=user_agency).count()  # noqa
+            bookings = Booking.objects.filter(trip__vehicle__agency=user_agency).count()  # noqa
+            tickets = Ticket.objects.filter(
+                booking__trip__vehicle__agency=user_agency).count()
+            transactions = Transaction.objects.filter(
+                booking__trip__vehicle__agency=user_agency).count()
+        categories = VehicleCategory.objects.all().count()
+
+        context = {
+            "total_users": total_users,
+            "agencies": agencies,
+            "vehicles": vehicles,
+            "seats": seats,
+            "trips": trips,
+            "categories": categories,
+            "bookings": bookings,
+            "tickets": tickets,
+            "transactions": transactions,
+        }
         return render(request, self.template, context)
