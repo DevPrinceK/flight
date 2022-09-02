63dc396e1d4cb4ec97d183a4d78bfe4c1f47b5e3
DevPrinceK <princesamuelpks@gmail.com>
2022-09-02 00:54:52 +0000

    Update signals.py

--- core/backend/signals.py
+++ core/backend/signals.py
@@ -1,8 +1,9 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-from backend.models import Wallet
+from backend.models import Ticket, Wallet
 from backend.models import Agency
+from backend.models import Transaction
 
 
 @receiver(post_save, sender=Agency)
@@ -22,3 +23,17 @@ def create_wallet(sender, instance, created, **kwargs):
             instance.wallet = wallet
             instance.save()
             print('Wallet created for labourer')
+
+
+@receiver(post_save, sender=Transaction)
+def generate_ticket(sender, instance, created, **kwargs):
+    '''Generate ticket for new transactions'''
+    if created == True:
+        if instance.status_code == "000":
+            Ticket.objects.create(transaction=instance)
+    else:
+        # generate ticket for existing transaction without a ticket
+        if instance.status_code == "000":
+            ticket = Ticket.objects.filter(transaction=instance).first()
+            if not ticket:
+                Ticket.objects.create(transaction=instance)
