9f4db25e6b548b3330d08fdcd54e047c4aafd09a
DevPrinceK <princesamuelpks@gmail.com>
2022-09-03 19:41:53 +0000

    Update util_functions.py

--- core/core/utils/util_functions.py
+++ core/core/utils/util_functions.py
@@ -16,6 +16,18 @@ def receive_payment(data):
     return response_data
 
 
+def make_payment(data):
+    ENDPOINT = 'https://payhubghana.io/api/v1.0/credit_mobile_account/'
+    headers = {
+        "Authorization": f"Token {settings.PAYHUB_SECRET_TOKEN}",
+    }
+
+    response = requests.post(ENDPOINT, data=data, headers=headers)
+    response_data = response.json()
+    print("From make_payment", response_data)
+    return response_data
+
+
 def get_transaction_status(transaction_id):
     ENDPOINT = 'https://payhubghana.io/api/v1.0/transaction_status'
     headers = {
