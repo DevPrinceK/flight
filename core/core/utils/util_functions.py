36aa1e76bf4daf57f676b254a15c28ae039cd0d9
DevPrinceK <princesamuelpks@gmail.com>
2022-09-08 18:11:43 +0000

    Update util_functions.py

--- core/core/utils/util_functions.py
+++ core/core/utils/util_functions.py
@@ -1,5 +1,4 @@
-
-
+import decimal
 import requests
 from core import settings
 
@@ -39,4 +38,31 @@ def get_transaction_status(transaction_id):
     response = requests.get(ENDPOINT, params=params, headers=headers)
     response_data = response.json()
     print('From get_transaction_status: ', response_data)
+    return response_data
+
+
+def get_api_wallet_balance():
+    ENDPOINT = 'https://payhubghana.io/api/v1.0/wallet_balance'
+    headers = {
+        "Authorization": f"Token {settings.PAYHUB_SECRET_TOKEN}",
+    }
+    payload = {
+        "wallet_id": settings.PAYHUB_WALLET_ID,
+    }
+    response = requests.get(ENDPOINT, headers=headers, params=payload)
+    try:
+        main_balance = response.json()['main_balance']
+        main_balance = decimal.Decimal(main_balance)
+    except KeyError:
+        main_balance = decimal.Decimal(0)
+    return main_balance
+
+
+def get_network_codes():
+    ENDPOINT = 'https://payhubghana.io/api/v1.0/network_codes'
+    headers = {
+        "Authorization": f"Token {settings.PAYHUB_SECRET_TOKEN}",
+    }
+    response = requests.get(ENDPOINT, headers=headers)
+    response_data = response.json()
     return response_data
