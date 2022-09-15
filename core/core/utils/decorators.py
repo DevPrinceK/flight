297a0412c072b0bff9c42f862311d2709c9dd799
DevPrinceK <princesamuelpks@gmail.com>
2022-09-15 18:08:35 +0000

    Update decorators.py

--- core/core/utils/decorators.py
+++ core/core/utils/decorators.py
@@ -8,13 +8,17 @@ class MustLogin(object):
 
     def __call__(self, request, *args,  **kwargs):
         if request.user.is_authenticated:
-            if (request.user.is_superuser or request.user.is_staff or request.user.is_agency_admin):  # noqa
+            if (request.user.is_superuser or request.user.is_staff):  # noqa
                 return self.original_method(request, *args, **kwargs)
+            elif (request.user.is_agency_admin):
+                if request.user.agency.is_approved:
+                    return self.original_method(request, *args, **kwargs)
+                else:
+                    messages.error(request, "Business not approved. Contact Admins!")  # noqa
+                    return redirect('accounts:login')
             else:
-                messages.error(
-                    request, "Access Denied!")
+                messages.error(request, "Access Denied!")  # noqa
                 return redirect('accounts:login')
         else:
-            messages.error(
-                request, 'Please login to continue.')
+            messages.error(request, 'Please login to continue.')  # noqa
             return redirect('accounts:login')
