d28368c9fe8c66b209add8a750c425e8494bcca4
DevPrinceK <princesamuelpks@gmail.com>
2022-09-15 18:05:44 +0000

    Update signals.py

--- core/backend/signals.py
+++ core/backend/signals.py
@@ -1,3 +1,6 @@
+import requests
+import array
+from core import settings
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
@@ -37,3 +40,49 @@ def generate_ticket(sender, instance, created, **kwargs):
             ticket = Ticket.objects.filter(transaction=instance).first()
             if not ticket:
                 Ticket.objects.create(transaction=instance)
+
+
+@receiver(post_save, sender=Agency)
+def notify_devprincek(sender, instance, created, **kwargs):
+    if created:
+        contact = instance.phone if instance.phone is not None else ''
+        name = instance.name if instance.name is not None else ''
+        email = instance.email if instance.email is not None else ''
+        subject = "New Agency Sign-up."
+        message = "Hey there! \n There is a new agency sign-up. \n Kindly confirm and approve!"
+
+        # For admins
+        agency_info = name + ' ' + contact + ' ' + email + ' \n'
+        body = subject.upper() + ' \n\n' + message
+        content = body + ' \n\n' + 'FROM: \n' + agency_info
+        send_sms("EasyGo Transport", content, ["0558366133"])
+
+        # for agencies
+        message = "Hey there! \n We have received your application to transact with EasyGo Trasport. \n We are doing our due deligence and will get your agency approved as soon as possible."
+        agency_info = name + ' ' + contact + ' ' + email + ' \n'
+        body = subject.upper() + ' \n\n' + message
+        content = body + ' \n\n' + 'AGENCY INFO: \n' + agency_info
+        send_sms("EasyGo Transport", content, ["0558366133"])
+    else:
+        if instance.is_approved:
+            # for agencies
+            message = "Congratulations! \n Your application to transact on EasyGO has been approved. \n Kindly log into the portal to start transacting."
+            subject = "Agency Approved!"
+            agency_info = name + ' ' + contact + ' ' + email + ' \n'
+            body = subject.upper() + ' \n\n' + message
+            content = body + ' \n\n' + 'AGENCY INFO: \n' + agency_info
+            send_sms("EasyGo Transport", content, ["0558366133"])
+
+
+def send_sms(sender: str, message: str, recipients: array.array):
+    header = {"api-key": settings.ARKESEL_API_KEY, 'Content-Type': 'application/json',
+              'Accept': 'application/json'}
+    SEND_SMS_URL = "https://sms.arkesel.com/api/v2/sms/send"
+    payload = {
+        "sender": sender,
+        "message": message,
+        "recipients": recipients
+    }
+    response = requests.post(SEND_SMS_URL, headers=header, json=payload)
+    print(response.json())
+    return response.json()
